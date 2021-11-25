bf151f889d821499d2dde95bdd1a67d5acd086a5
ParenkovLocal <parenkov.fedor@gmail.com>
2021-11-25 12:16:16 +0400

    Added test scenario

--- src/test/java/parenkov/tests/WebShopTests.java
+++ src/test/java/parenkov/tests/WebShopTests.java
@@ -3,27 +3,30 @@ package parenkov.tests;
 import com.codeborne.selenide.Configuration;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import org.openqa.selenium.Cookie;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import parenkov.config.App;
 
+import static com.codeborne.selenide.Condition.matchText;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
 import static io.qameta.allure.Allure.step;
+import static org.hamcrest.Matchers.*;
 
 public class WebShopTests {
 
     @BeforeAll
     static void configureBaseUrl() {
         RestAssured.baseURI = App.config.apiUrl();
-        Configuration.baseUrl = App.config.webUrl();
+//        Configuration.baseUrl = App.config.webUrl();
     }
 
     @Test
@@ -53,8 +56,8 @@ public class WebShopTests {
 /*
 Preconditions: открыта главная страница http://demowebshop.tricentis.com/
 1. Перейти в раздел Computers -> Desktops
-2. Выбрать значение "Over 1200.00" для фильтра по цене
-3. Перейти на страницу компьютера с ценой 1800
+2. Выбрать значение фильтра цены "Over 1200.00"
+3. Перейти на карточку товара с ценой 1800
 4. Выбрать максимальные характеристики компьютера:
     - Processor - Fast;
     - RAM - 8GB;
@@ -64,33 +67,32 @@ Preconditions: открыта главная страница http://demowebshop
 6. Нажать Add to cart
 7. Перейти в Shopping cart
 8. Проверить наличие товара в Shopping cart
-9. Удалить товар из Shopping cart:
+
+Другой кейс
+1. Удалить товар из Shopping cart:
     - отметить чек-бокс Remove;
     - нажать Update shopping cart
-10. Проверить отсутствие товара в Shopping cart
+2. Проверить отсутствие товара в Shopping cart
  */
 
 
-
-
-
-
     @Test
-    @DisplayName("Successful authorization to some demowebshop (API + UI)")
-    void loginWithCookieTest() {
-        step("Get cookie by api and set it to browser", () -> {
-//                    given()
-//                            .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-//                            .formParam("Email", App.config.userEmail())
-//                            .formParam("Password", App.config.userPassword())
-//                            when()
-            String response =
-                    get("/desktops")
-                            .then()
-                            .statusCode(200)
-                            .extract().body().asString();
-            System.out.println(response);
+    @DisplayName("Добавление товара с пользовательскими параметрами в корзину")
+    void shoppingCartTest() {
+        step("Перейти в раздел Computers -> Desktops", () -> {
+            get("/desktops")
+                    .then()
+                    .statusCode(200)
+                    .body("html.head.title", equalTo("Demo Web Shop. Desktops"));
         });
+
+        step("Выбрать значение фильтра цены 'Over 1200.00'", () -> {
+            get("/desktops?price=1200-")
+                    .then()
+                    .statusCode(200)
+                    .body("html.head.title", equalTo("Demo Web Shop. Desktops"));
+        });
+
     }
 
 //            step("Open minimal content, because cookie can be set when site is opened", () ->
@@ -139,8 +141,6 @@ Preconditions: открыта главная страница http://demowebshop
 //        step("Verify successful authorization", () ->
 //                $(".account").shouldHave(text(App.config.userEmail())));
 //    }
-
-
 
 
 }
